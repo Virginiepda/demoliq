7239db89c3e1fd04b191ed2498091e27aae4552c
Virginie <virginie@pineaudeandrade.com>
2019-01-29 10:05:57 +0100

    QuestionController list

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -14,10 +14,14 @@ class QuestionController extends AbstractController
     public function list()
     {
     //il créé une instance de notre classe, pour aller chercher les infos dans la base
+        //il nous permet de faire des SELECT
         $questionRepository=$this->getDoctrine()->getRepository(Question::class);
         //$questions = $questionRepository->findAll(); //ça retourne un tableau d'objets question
 
+
+
         //on va plutôt faire ceci
+        //équivaut à SELECT * FROM question WHERE status = 'debating' ORDER BY supports DESC LIMIT 1000
         $questions = $questionRepository->findBy(
             ['status' => 'debating'], //where
             ['supports' => 'DESC'], //order by
@@ -27,9 +31,17 @@ class QuestionController extends AbstractController
 
         //dd($questions);
 
-
-        return $this->render('question/list.html.twig');
+//pour passer les arguments dans twig afin de pouvoir les afficher, on les passe dans la fonction render en 2ème argument
+        return $this->render('question/list.html.twig', [
+            "questions" => $questions,
+        ]);
     }
+
+
+//    public function detail()
+//    {
+//        $questionRepository=$this
+//    }
 
 
 
